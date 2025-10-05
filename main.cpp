71a2f0e5c0b2e77abc9e78055a84c4893ed7f692
Tanmayee Chalamalasetti <tanmayeesc@MacBookPro.attlocal.net>
2025-10-05 16:59:45 -0700

    Push 3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
+// COMSC210 | Lab 18 | Tanmayee Chalamalasetti
+// IDE Used: VS Code
+
 #include <iostream>
 #include <string>
 using namespace std;
 
+const float min = 0.0;
+const float max = 5.0;
+
+// Struct with review attributes
 struct ReviewNode {
   float rating;
+  string comment;
+  ReviewNode* next;
+};
 
-}
+class ReviewList {
+ private:
+  ReviewNode* head;
+  ReviewNode* tail;
+  bool addToHead;
 
-int main() {
-  return 0;
-}
+ public:
+  ReviewList(bool mode) {
+    head = nullptr;
+    tail = nullptr;
+  }
+};
+
+int main() { return 0; }
